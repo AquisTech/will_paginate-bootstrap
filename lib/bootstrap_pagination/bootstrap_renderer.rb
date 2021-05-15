d5b72674423d6b68fa082372727d61137b5cbacc
AquisTech <anand.bait@gmail.com>
2021-05-15 02:16:42 -0400

    Bootstrap 4.6.0 compatibility changes

--- lib/bootstrap_pagination/bootstrap_renderer.rb
+++ lib/bootstrap_pagination/bootstrap_renderer.rb
@@ -8,14 +8,13 @@ module BootstrapPagination
     def to_html
       list_items = pagination.map do |item|
         case item
-          when Fixnum
-            page_number(item)
-          else
-            send(item)
+        when Integer
+          page_number(item)
+        else
+          send(item)
         end
       end.join(@options[:link_separator])
-
-      tag("ul", list_items, class: ul_class)
+      tag(:nav, tag("ul", list_items, class: ul_class))
     end
 
     def container_attributes
@@ -28,9 +27,9 @@ module BootstrapPagination
       link_options = @options[:link_options] || {}
 
       if page == current_page
-        tag("li", tag("span", page), class: "active")
+        tag("li", tag("span", page, class: 'page-link'), class: "page-item active")
       else
-        tag("li", link(page, page, link_options.merge(rel: rel_value(page))))
+        tag("li", link(page, page, link_options.merge(rel: rel_value(page), class: 'page-link')), class: 'page-item')
       end
     end
 
@@ -38,24 +37,24 @@ module BootstrapPagination
       link_options = @options[:link_options] || {}
 
       if page
-        tag("li", link(text, page, link_options), class: classname)
+        tag("li", link(text, page, link_options.merge(class: 'page-link')), class: classname)
       else
-        tag("li", tag("span", text), class: "%s disabled" % classname)
+        tag("li", tag("span", text, class: 'page-link'), class: "#{classname} disabled")
       end
     end
 
     def gap
-      tag("li", tag("span", ELLIPSIS), class: "disabled")
+      tag("li", tag("span", ELLIPSIS, class: 'page-link'), class: "page-item disabled")
     end
 
     def previous_page
       num = @collection.current_page > 1 && @collection.current_page - 1
-      previous_or_next_page(num, @options[:previous_label], "prev")
+      previous_or_next_page(num, @options[:previous_label], "page-item prev")
     end
 
     def next_page
       num = @collection.current_page < @collection.total_pages && @collection.current_page + 1
-      previous_or_next_page(num, @options[:next_label], "next")
+      previous_or_next_page(num, @options[:next_label], "page-item next")
     end
 
     def ul_class
